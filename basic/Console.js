bd542fb1f7c0fd64f462cc44546815194836e603
hanyulrich <126761282+gimhanyul@users.noreply.github.com>
2024-02-26 17:08:24 +0900

    Add files via upload

---
+++ basic/Console.js
@@ -0,0 +1,59 @@
+console.assert()
+//첫 번째 매개변수가 false인 경우 메시지와 스택 추적을 출력합니다.
+
+console.clear()
+//콘솔의 내용을 지웁니다.
+
+console.count()
+//주어진 레이블로 메서드를 호출한 횟수를 출력합니다.
+
+console.countReset()
+//주어진 라벨의 횟수를 초기화합니다.
+
+console.debug()
+//debug 중요도로 메시지를 출력합니다.
+
+console.dir()
+//주어진 JavaScript 객체의 속성 목록을 상호작용 가능한 형태로 표시합니다. 속성 값이 다른 객체라면 펼쳐서 살펴볼 수 있습니다.
+
+console.dirxml()
+//객체를 XML/HTML 요소 형태로 나타낼 수 있으면 그렇게 표시하고, 아닐 경우 JavaScript 객체 형태로 표시합니다.
+
+console.error()
+//오류 메시지를 출력합니다. 추가 매개변수와 함께 문자열 치환을 사용할 수 있습니다.
+
+console.group()
+//새로운 인라인 그룹을 생성해, 이후 모든 출력을 한 단계 들여씁니다. 그룹을 나오려면 groupEnd()를 호출하세요.
+
+console.groupCollapsed()
+//새로운 인라인 그룹을 생성해, 이후 모든 출력을 한 단계 들여씁니다. 그러나 group()과 달리, groupCollapsed()로 생성한 그룹은 처음에 접혀 있습니다. 그룹을 나오려면 groupEnd()를 호출하세요.
+
+console.groupEnd()
+//현재 인라인 그룹을 나옵니다.
+
+console.info() (en-US)
+//정보 메시지를 출력합니다. 추가 매개변수와 함께 문자열 치환을 사용할 수 있습니다.
+
+console.log()
+//일반 메시지를 출력합니다. 추가 매개변수와 함께 문자열 치환을 사용할 수 있습니다.
+
+console.profile() (en-US)
+//브라우저의 내장 프로파일러(Firefox 성능 측정 도구 등)를 실행합니다. 선택 사항으로 프로파일에 이름을 붙일 수 있습니다.
+
+console.profileEnd() (en-US)
+//프로파일러를 멈춥니다. 프로파일 결과는 브라우저의 성능 측정 도구(Firefox 성능 측정 도구 등)에서 확인할 수 있습니다.
+
+console.table()
+//표 형태의 데이터를 표에 그립니다.
+
+console.time()
+//주어진 이름의 타이머를 실행합니다. 하나의 페이지에서는 최대 10,000개의 타이머를 동시에 실행할 수 있습니다.
+
+console.timeEnd()
+//지정한 타이머를 멈추고, 소요시간을 출력합니다.
+
+console.trace()
+//스택 추적을 출력합니다.
+
+console.warn()
+//경고 메시지를 출력합니다. 추가 매개변수와 함께 문자열 치환을 사용할 수 있습니다.
